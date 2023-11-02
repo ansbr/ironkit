9e064185aa3310723c05233dae0d9d7fd6180c73
anbork <anbork@example.com>
2023-11-02 09:47:07 +0300

    Update discord ux

--- pages/discord.tsx
+++ pages/discord.tsx
@@ -12,9 +12,10 @@ const Discord = () => {
     </Head>
     <Layout>
       <ol className="list-group list-group-numbered w-100 mx-auto" style={{maxWidth: '500px'}}>
-        <li className="list-group-item d-flex justify-content-between align-items-start">
+        <li className={`list-group-item d-flex justify-content-between align-items-start ${discordUser ? 'disabled' : '' }`}>
           <div className="ms-2 me-auto">
             <div className="fw-bold">Sign In with Discord</div>
+            {!discordUser && <>Authorize yourself using Discord platform</>}
             {discordUser && <>Welcome {discordUser}</>}
           </div>
           {!discordUser && <button className="btn btn-primary btn-sm py-0" onClick={signInDiscord}>
@@ -24,10 +25,11 @@ const Discord = () => {
             Done
           </button>}
         </li>
-        <li className="list-group-item d-flex justify-content-between align-items-start">
+        <li className={`list-group-item d-flex justify-content-between align-items-start ${!discordUser ? 'pe-none opacity-75' : '' } ${discordUser && isJoinedDiscord? 'disabled' : '' }`}>
           <div className="ms-2 me-auto">
             <div className="fw-bold">Join our <a href="https://discord.gg/8axaNyd9" target="_blank">Discord channel</a></div>
-            Verify that you have joined
+            {!discordUser || !isJoinedDiscord && <>Verify that you have joined</>}
+            {discordUser && isJoinedDiscord && <>You are successfuliy verified</>}
           </div>
           {!isJoinedDiscord && <button className="btn btn-primary btn-sm py-0" onClick={verifyJoinDiscord}>
             Verify
@@ -36,10 +38,10 @@ const Discord = () => {
             Done
           </button>}
         </li>
-        <li className="list-group-item d-flex justify-content-between align-items-start">
+        <li className={`list-group-item d-flex justify-content-between align-items-start  ${!discordUser || !isJoinedDiscord ? 'pe-none opacity-75' : '' }`}>
           <div className="ms-2 me-auto">
             <div className="fw-bold">Congratulations!</div>
-            Now you can claim your free chest.
+            Now you can claim your free chest
           </div>
           {<button className="btn btn-primary btn-sm py-0" onClick={() => {}}>
             Claim
