38d1b505cd4ae6c41ceebe2c44e6605b2e8de818
anbork <anbork@example.com>
2023-11-03 00:56:17 +0300

    Update twitter endpoint

--- pages/api/twitter/verify.ts
+++ pages/api/twitter/verify.ts
@@ -1,7 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { createClient } from '@supabase/supabase-js'
+import { TwitterApi } from 'twitter-api-v2';
 
 const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
+
+const client = new TwitterApi(process.env.TWITTER_BEARER_TOKEN!);
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { method } = req;
@@ -9,7 +12,18 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     case 'POST':
       const { data, error } = await supabase.auth.getUser(req.body.access_token as string);
       let twitterUserId: string | undefined = data?.user?.identities?.find(x => x.provider === 'twitter')?.identity_data?.provider_id;
-    
+
+      if (twitterUserId) {
+        const friendships = await client.v1.friendships({ user_id: [ twitterUserId ] });
+  
+        for (const friendship of friendships) {
+          console.log(JSON.stringify(friendship));
+          // if (friendship.connections.includes('following')) {
+          //   console.log(`You are following @${friendship.screen_name}.`);
+          // }
+        }
+      }
+
       if (error) {
         res.status(502).json({ error });
       } else {
