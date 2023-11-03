b0e9322958f26c00e688ee8f8167fdb0c2295364
anbor <an@bor.com>
2023-11-03 16:51:21 +0200

    Update Twitter without result

--- pages/api/twitter/verify.ts
+++ pages/api/twitter/verify.ts
@@ -4,30 +4,32 @@ import { TwitterApi } from 'twitter-api-v2';
 
 const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
 
-const client = new TwitterApi(process.env.TWITTER_BEARER_TOKEN!);
+const credentials = { 
+  appKey: process.env.TWITTER_API_KEY!,
+  appSecret: process.env.TWITTER_API_KEY_SECRET!,
+  accessToken: process.env.TWITTER_ACCESS_TOKEN!,
+  accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET!
+}
+
+const twitterClient = new TwitterApi(credentials);
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { method } = req;
   switch (method) {
     case 'POST':
       const { data, error } = await supabase.auth.getUser(req.body.access_token as string);
-      let twitterUserId: string | undefined = data?.user?.identities?.find(x => x.provider === 'twitter')?.identity_data?.provider_id;
+      const twitterData: {[key: string]: string} | undefined = data?.user?.identities?.find(x => x.provider === 'twitter')?.identity_data;
 
-      if (twitterUserId) {
-        const friendships = await client.v1.friendships({ user_id: [ twitterUserId ] });
-  
-        for (const friendship of friendships) {
-          console.log(JSON.stringify(friendship));
-          // if (friendship.connections.includes('following')) {
-          //   console.log(`You are following @${friendship.screen_name}.`);
-          // }
-        }
+      if (twitterData) {
+        const { provider_id: twitterUserId, user_name: twitterUserName } = twitterData
+        // that not working without paid access to Twitter Api
+        const friendships = await twitterClient.v1.friendships({ user_id: twitterUserId });
       }
 
       if (error) {
         res.status(502).json({ error });
       } else {
-        res.status(200).json({ twitterUserId: twitterUserId });
+        res.status(200).json({ isMember: false });
       }
       break;
     default:
